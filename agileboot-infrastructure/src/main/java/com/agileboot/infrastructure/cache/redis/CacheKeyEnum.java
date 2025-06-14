d71ab8f31833a037cff4133bb3d6ce7ea7df40c0
lucianV5 <2319556364@qq.com>
2025-06-14 22:31:30 +0800

    Update CacheKeyEnum.java
    redis添加机构缓存key

--- agileboot-infrastructure/src/main/java/com/agileboot/infrastructure/cache/redis/CacheKeyEnum.java
+++ agileboot-infrastructure/src/main/java/com/agileboot/infrastructure/cache/redis/CacheKeyEnum.java
@@ -17,7 +17,8 @@ public enum CacheKeyEnum {
     ROLE_ENTITY_KEY("role_entity:", 60, TimeUnit.MINUTES),
     POST_ENTITY_KEY("post_entity:", 60, TimeUnit.MINUTES),
     ROLE_MODEL_INFO_KEY("role_model_info:", 60, TimeUnit.MINUTES),
-
+    DEPT_ENTITY_KEY("dept_entity:", 60, TimeUnit.MINUTES),
+    
     ;
 
 
